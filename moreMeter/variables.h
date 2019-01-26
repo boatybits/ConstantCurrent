24dca390fd654388cc8021224cf4f6394ea80ff6
PB <bitsboaty@gmail.com>
2019-01-26 10:58:32 +0000

    Update variables.h

--- moreMeter/variables.h
+++ moreMeter/variables.h
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include <INA219.h>                  // https://github.com/flav1972/ArduinoINA219
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>            // MQTT library
+#include <PubSubClient.h>            // MQTT librarycc
 
 
 //                         Variables
